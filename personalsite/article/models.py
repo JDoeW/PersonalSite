ae9636ca2c61065de5f4886c2f96a3e6d9cd7107
JDoeW <wygithub@gmail.com>
2020-12-28 21:01:51 +0800

    补充article部分内容(模型、映射和静态文件等)

--- personalsite/article/models.py
+++ personalsite/article/models.py
@@ -5,15 +5,19 @@ from django.utils import timezone
 
 class ArticlePost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-	title = models.CharField(max_length=100)
-	body = models.TextField()
-	created = models.DateTimeField(default=timezone.now)
-	# auto_now=True 指定每次数据更新写入时间
-	updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField(blank=True)
+    created = models.DateTimeField(default=timezone.now)
+    # auto_now=True 指定每次数据更新写入时间
+    updated = models.DateTimeField(auto_now=True)
 
-	class Meta:
-	    ordering = ('-created',)
+    # Meta提供元数据, 元数据表示任何不是字段的东西。
+    # 比如排序(ordering), 单复数(verbose_name, verbose_name_plural), 数据库表名(db_table)
+    # ordering = ('-created') -> created 按创建时间 - 倒序 
+    # 在网站首页展示, 最新文章呈现在最上面
+    class Meta:
+        ordering = ('-created',)
 
-	class __str__(self):
-	    return self.title
+    def __str__(self):
+        return self.title
 
---
+++ personalsite/article/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# 部署应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    path('article-list/', views.article_list, name='article_list'),
+]
+
