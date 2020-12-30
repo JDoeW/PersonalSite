828e4755398bea369212e3ab5f34ee89f74c10ca
JDoeW <wygithub@gmail.com>
2020-12-30 21:06:55 +0800

    新增Markdown功能

--- personalsite/article/views.py
+++ personalsite/article/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
@@ -12,6 +13,13 @@ def article_list(request):
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 语法、表格等常用扩展
+        #'markdown.extensions.extra',
+        # 语法高亮扩展
+        #'markdown.extensions.codehilite',
+    ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
 
